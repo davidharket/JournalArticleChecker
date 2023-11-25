c825200bee0aef10943fca9fc6d2770b84341d58
davidharket <112109685+davidharket@users.noreply.github.com>
2023-11-25 13:38:26 +1000

    Update requests_module.py

--- requests_module.py
+++ requests_module.py
@@ -3,7 +3,16 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
 def get_all_links(url):
-    # ... existing code for get_all_links ...
+    """
+    Retrieve all hyperlinks from the specified URL.
+    Args:
+    url (str): The URL from which to scrape the hyperlinks.
+    Returns:
+    list: A list of hyperlinks found on the specified webpage.
+    """
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    return [link.get('href') for link in soup.find_all('a') if link.get('href')]
 
 def fetch_and_parse(url):
     try:
